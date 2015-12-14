1ac81826637c65a99669955b236641665f77e4d5
Brian Hoffman <brianchoffman@gmail.com>
2015-12-14 16:36:24 -0500

    Add menu,tray,auto-updater to Electron deps

--- valiquire-redirect.js
+++ valiquire-redirect.js
@@ -1,5 +1,5 @@
 'use strict';
-var electronModules = ['app', 'browser-window', 'crash-reporter', 'remote', 'shell', 'clipboard'];
+var electronModules = ['app', 'browser-window', 'crash-reporter', 'remote', 'shell', 'clipboard', 'tray', 'menu', 'auto-updater'];
 
 module.exports = function redirect(request) {
   // Tell valiquire to ignore these modules
